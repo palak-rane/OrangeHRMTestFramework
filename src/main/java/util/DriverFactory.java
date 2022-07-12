3228254ea5e92edec16354027a5d7d151b4aea2e
palak-rane <107403370+palak-rane@users.noreply.github.com>
2022-07-12 19:38:12 +0100

    Framework

---
+++ src/main/java/util/DriverFactory.java
@@ -0,0 +1,42 @@
+package util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DriverFactory {
+        public static WebDriver driver = null;
+
+        public static WebDriver getDriver(String browser){
+        if (driver==null)
+        {
+            if(browser.equals("chrome")) {
+                System.setProperty("webdriver.chrome.driver" , "drivers/chromedriver.exe");
+                driver =new ChromeDriver();
+            } else if (browser.equals("ie")) {
+                System.setProperty("webdriver.ie.driver" , "drivers/IEdriverServer.exe");
+                driver =new InternetExplorerDriver();
+            }
+        }
+            return driver;
+        }
+        public  static void LaunchUrl(String url){
+            driver.get(url);
+        }
+        public static void CloseDriver(){
+            driver.close();
+        }
+        public void waitElementToBeVisible(WebElement webElement)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOf(webElement));
+        }
+
+}
+
